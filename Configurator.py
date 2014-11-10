029edc1a1f321dea10eef0a21afe0ff996e8a13f
mikeraf <m1k3raf@gmail.com>
2014-11-10 22:34:56 +0200

    Objects for EntropyEstimator and EntropicSolver
    The solver not working yet.
    The RefinedEntropyEstimator should be filled in.

--- Configurator.py
+++ Configurator.py
@@ -9,7 +9,7 @@ import operator
 import math
 from Cube import *
 
-__all__ = ['randomize', 'get_configuration_entropy']
+__all__ = ['randomize', 'BasicEntropyEstimator', 'RefinedEntropyEstimator']
 def randomize(cube, nsteps):
     '''Apply :nsteps: random rotations on :cube:'''
     for i in xrange(nsteps):
@@ -23,39 +23,60 @@ def _random_rotation_params():
     n = random.randint(1,3)
     return f,n
 
+#############################################################
 def product(iterable):
+    '''return the product of all elements in :iterable:
+    >>> l = [1,2,3,4]
+    >>> product(l)
+    24
+    
+    '''
     return reduce(operator.mul, iterable, 1)
+#------------------------------------------------------------
+class BasicEntropyEstimator(object):
+    def get_configuration_entropy(self, cube):
+        '''calculate some estimator to cubes' configuration entropy
+        
+        >>> c = Cube()
+        >>> ee = BasicEntropyEstimator()
+        >>> ee.get_configuration_entropy(c)
+        0.0
+        
+        '''
+        prod = 1
+        for i in xrange(6):
+            prod *= self._get_face_entropy(cube.get_face(i))
+        return math.log(prod)
+    #------------------------------------------------------------
+    
+    def _get_face_entropy(self, face):
+        '''
+        >>> f = Face(1)
+        >>> ee = BasicEntropyEstimator()
+        >>> ee._get_face_entropy(f)
+        1
+        >>> f = Face(range(9))
+        >>> ee._get_face_entropy(f)
+        362880
+        
+        '''        
+        vals = face.get_vals()
+        d = {}
+        for v in vals:
+            d[v] = d.get(v,0) + 1
+        return math.factorial(9)/product([math.factorial(i) for i in d.values()])
+    #------------------------------------------------------------    
 
-def get_face_entropy(face):
-    '''
-    >>> f = Face(1)
-    >>> get_face_entropy(f)
-    1
-    >>> f = Face(range(9))
-    >>> get_face_entropy(f)
-    362880
-    
-    '''
-    
-    vals = face.get_vals()
-    d = {}
-    for v in vals:
-        d[v] = d.get(v,0) + 1
-    return math.factorial(9)/product([math.factorial(i) for i in d.values()])
-
-def get_configuration_entropy(cube):
-    '''calculate some estimator to cubes' configuration entropy
-    
-    >>> c = Cube()
-    >>> get_configuration_entropy(c)
-    0.0
-    
-    '''
-    prod = 1
-    for i in xrange(6):
-        prod *= get_face_entropy(cube.get_face(i))
-    return math.log(prod)
-
+class RefinedEntropyEstimator(BasicEntropyEstimator):
+    def _get_face_entropy(self, face):
+        #TODO: currently is equal to BasicEntropyEstimator, change it!
+        vals = face.get_vals()
+        d = {}
+        for v in vals:
+            d[v] = d.get(v,0) + 1
+        return math.factorial(9)/product([math.factorial(i) for i in d.values()])        
+        
+  
 if __name__ == '__main__':
     import doctest
     doctest.testmod()    
--- entropic_solver.py
+++ entropic_solver.py
@@ -7,53 +7,122 @@ Created on Sun Nov 2 22:00:00 2014
 from Configurator import *
 from Cube import *
 
-def try_reduce_entropy(cube, cur_entropy):
-    good_moves = {}
-    for f in xrange(6):
-        for n in xrange(1,4,1):
-            cube.rotate(f,n)
-            new_entropy = get_configuration_entropy(cube)
-            if new_entropy < cur_entropy:
-                good_moves[(f,n)] = new_entropy
+class EntropicSolver(object):
+    def __init__(self, cube, entropy_estimator):
+        self.cube = cube
+        self.entropy_estimator = entropy_estimator
+        
+        self.cur_entropy = entropy_estimator.get_configuration_entropy(cube)
+        self.moves = []   
+    #---------------------------------------------------------
+    def _try_reduce_entropy(self):
+        cube = self.cube
+        estimator = self.entropy_estimator
+        cur_entropy = self.cur_entropy
+        
+        good_moves = {}
+        for f in xrange(6):
+            for n in xrange(1,4,1):
+                cube.rotate(f,n)
+                new_entropy = estimator.get_configuration_entropy(cube)
                 # revert rotation to check for better
-                cube.rotate(f,-n)
-            else:
-                continue
+                cube.rotate(f,-n)       
+                if new_entropy < cur_entropy:
+                    good_moves[(f,n)] = new_entropy
+                else:
+                    continue
+                pass
             pass
-        pass
-    if good_moves:
-        best_move = min(good_moves.keys(), key=lambda k: good_moves[k])
-        cube.rotate(*best_move)
-        return True
-    else:
-        return False
-
-def main():
-    c = Cube()
-    randomize(c, 100)
-    print c
-    for i in xrange(100):
-        entropy = get_configuration_entropy(c)
+        if good_moves:
+            best_move = min(good_moves.keys(), key=lambda k: good_moves[k])
+            cube.rotate(*best_move)
+            self.moves.append(best_move)
+            self.cur_entropy = estimator.get_configuration_entropy(cube)
+            return True
+        else:
+            return False           
+    #---------------------------------------------------------
+    def _try_backtracking(self, cur_entropy, face=-1, depth=3, recuring=0):
+        cube = self.cube
+        estimator = self.entropy_estimator
+        
+        moves = []
+        for f in xrange(6):
+            for n in xrange(1,4,1): 
+                if face == f:
+                    continue
+                cube.rotate(f,n)
+                if depth > 0:
+                    sub_moves = self._try_backtracking(cur_entropy, f, depth-1, 
+                                                       recuring=1)
+                    if sub_moves:
+                        moves.append((f,n))                
+                        moves.extend(sub_moves)
+                else: 
+                    new_entropy = estimator.get_configuration_entropy(cube)
+                    if new_entropy < cur_entropy:
+                        moves.append((f,n))
+                cube.rotate(f,-n)
+                if moves:
+                    if recuring:
+                        return moves
+                    else:
+                        break
+                    pass
+                pass
+            pass
+                
+        if not recuring and moves:
+            self.moves.extend(moves)
+            for move in moves:
+                cube.rotate(*move)
+            self.cur_entropy = estimator.get_configuration_entropy(cube)
+        return moves    
+    #---------------------------------------------------------
+    def _perform_single_step(self, max_backtracking):
+        cube = self.cube
+        estimator = self.entropy_estimator        
+        
+        entropy = self.cur_entropy
         print 'current entropy is:', entropy
         if entropy == 0.0:
             return
         
-        reduced = try_reduce_entropy(c, entropy)    
+        reduced = self._try_reduce_entropy()    
         if reduced:
             print 'succeeded to reduce!'
-            continue
+            return
         else:
-            print 'trying backtracking...'
-            break
-            #reduced = try_backtracking(c, entropy)
-            #if reduced:
-                #print 'success'
-            #else:       
-                #print 'Failed!'
-                #break
-            
-            #pass
-        pass
+            for depth in range(1, max_backtracking+1):
+                print 'Trying backtracking: depth=%d...'%depth
+                steps = self._try_backtracking(entropy, depth=depth)
+                if steps:
+                    print 'Success!'
+                else:       
+                    print 'Failed!' 
+                    pass
+                pass      
+        
+        
+    #---------------------------------------------------------    
+    def solve(self, max_steps=100, max_backtracking=3):
+        if self.cur_entropy == 0.0:
+            return
+        finished = False
+        step = 0
+        while not finished and step < max_steps:
+            finished = self._perform_single_step(max_backtracking)
+        if finished:
+            print "Success!"
+        else:
+            print "Fail!"
+    
+def main():
+    c = Cube()
+    randomize(c, 5)
+    print c
+    solver = EntropicSolver(c, BasicEntropyEstimator())
+    solver.solve()
     print c
 
 if __name__ == '__main__':
